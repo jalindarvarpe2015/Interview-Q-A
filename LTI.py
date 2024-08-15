c59c69eeff2cb2ad224da9df818ed59c71c3cee8
Varpe Jalindar <jalindarvarpe2015@gmail.com>
2024-08-15 12:25:52 +0530

    adding new chanhes

--- LTI.py
+++ LTI.py
@@ -1,1 +1,11 @@
-print('HELLOW WORLD')
+# Q-1 : He provides me this dictionary and he want only keys having valuei.e skipping the "None"
+
+d = {'a':1,'b':None,'c':"ABC",'d':"123"}
+# output: a, c, d
+Output = [key for key, value in d.items() if value is not None]
+print(Output)
+
+
+
+
+
